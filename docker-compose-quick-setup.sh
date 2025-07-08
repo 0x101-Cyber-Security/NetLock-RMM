f52ca0ade0aaf86860dafa7896e4a472236c6abc
Nico Mak <69112587+0x100001@users.noreply.github.com>
2025-07-08 15:28:06 +0200

    Update docker-compose-quick-setup.sh
    Fixed wrong domain placement

--- docker-compose-quick-setup.sh
+++ docker-compose-quick-setup.sh
@@ -597,7 +597,7 @@ sudo tee /home/netlock/server/appsettings.json > /dev/null <<EOF
     "Enabled": $le_enabled,
     "AcceptTermsOfService": true,
     "DomainNames": [
-      "$le_domain"
+      "$server_domain"
     ],
     "EmailAddress": "$le_email",
     "AllowedChallengeTypes": "Http01",
